c690e45a28de0dc98bb7477c31d9bedca8c8dae9
gandhi56 <gandhi21299@gmail.com>
2020-04-17 01:29:32 -0600

    json fetch in progress

--- app/src/main/java/com/example/rubberduck/MainActivity.kt
+++ app/src/main/java/com/example/rubberduck/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.rubberduck
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -48,11 +52,30 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         replaceFragment(UserFragment()) // default fragment on screen
         bottomNavigation.menu.findItem(R.id.user).setChecked(true)
+
+        var receivedJson = ""
+        val getJson: (String)->Unit = {s : String -> receivedJson = s}
+        getUserData("https://codeforces.com/api/user.info?handles=gandhi21299", getJson)
+        println("Respone: ${receivedJson}")
     }
 
     private fun replaceFragment(fragment: Fragment){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragmentContainer, fragment)
         fragmentTransaction.commit()
+    }
+
+    private fun getUserData(url: String, callback: (String)->Unit) : Unit{
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String>{
+                    response ->
+                callback(response)
+            },
+            Response.ErrorListener{
+                callback("ERROR")
+            })
+        queue.add(stringRequest)
     }
 }
--- app/src/main/java/com/example/rubberduck/StatsFragment.kt
+++ app/src/main/java/com/example/rubberduck/StatsFragment.kt
@@ -5,18 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 /**
  * A simple [Fragment] subclass.
  */
 class StatsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stats, container, false)
     }
-
 }
