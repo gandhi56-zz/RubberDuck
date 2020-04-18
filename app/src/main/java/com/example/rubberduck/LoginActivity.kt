2610a4f47c4185e62ea8fc3ddd0b7e509d7dd27d
gandhi56 <gandhi21299@gmail.com>
2020-04-17 23:17:06 -0600

    refactoring, autohide keyboard on button click, added toasts, POINT

--- app/src/main/java/com/example/rubberduck/LoginActivity.kt
+++ app/src/main/java/com/example/rubberduck/LoginActivity.kt
@@ -1,22 +1,24 @@
 package com.example.rubberduck
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.json.JSONObject
 
 
 const val EMAIL_MESSAGE = "com.example.rubberduck.EMAIL_MESSAGE"
-
-// https://codeforces.com/api/user.info?handles=DmitriyH;Fefer_Ivan
-// https://codeforces.com/api/user.info?handles=gandhi56
 
 @Suppress("UNREACHABLE_CODE")
 class LoginActivity : AppCompatActivity() {
@@ -31,23 +33,24 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun startMainActivity(view: View){
+        hide_keyboard()
         if (validateInput()){
             getUserProfile().execute()
             println("done")
         }
         else{
-
+            Toast.makeText(this,"Please enter a codeforces handle",Toast.LENGTH_SHORT).show()
         }
     }
 
-    internal inner class getUserProfile : AsyncTask<Void, Void, String>(){
+    internal inner class getUserProfile : AsyncTask<Context, Void, String>(){
         override fun onPreExecute() {
             super.onPreExecute()
             progBar.setVisibility(View.VISIBLE)
         }
 
         @SuppressLint("WrongThread")
-        override fun doInBackground(vararg params: Void?): String {
+        override fun doInBackground(vararg params: Context): String {
             val client = OkHttpClient()
             val url = "https://codeforces.com/api/user.info?handles=" + getHandle()
             val request = Request.Builder().url(url).build()
@@ -55,7 +58,14 @@ class LoginActivity : AppCompatActivity() {
             val json = response.body()?.string().toString()
             val lbl = findViewById<TextView>(R.id.textView2)
             lbl.text = json
-            return json
+
+            val jsonObj = JSONObject(json)
+            val status = jsonObj.getString("status")
+            if (status == "FAILED"){
+                // TODO reason("display toast that the input handle is invalid")
+//                Toast.makeText(applicationContext,"Please enter a codeforces handle",Toast.LENGTH_SHORT).show()
+            }
+            return "foo"
         }
 
         override fun onPostExecute(result: String?) {
@@ -65,7 +75,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun validateInput(): Boolean{
-        return true
+        return getHandle().isNotEmpty()
     }
 
     private fun getHandle(): String{
@@ -73,4 +83,12 @@ class LoginActivity : AppCompatActivity() {
         return nameTxt.text.toString()
     }
 
+    private fun hide_keyboard(){
+        val view = this.currentFocus
+        if (view != null){
+            val hideMe = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            hideMe.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+    }
 }
