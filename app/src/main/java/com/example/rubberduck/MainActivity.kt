f229ddc353efa91ea541f9cd3b3ea022c233b2dc
gandhi56 <gandhi21299@gmail.com>
2020-04-21 01:29:11 -0600

    added rating history chart, POINT

--- app/src/main/java/com/example/rubberduck/MainActivity.kt
+++ app/src/main/java/com/example/rubberduck/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.rubberduck
 
+import android.content.Intent
 import android.content.Intent.EXTRA_USER
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -10,10 +13,12 @@ import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
+    var user: User? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val user = intent.getSerializableExtra(EXTRA_USER) as User
+        user = intent.getSerializableExtra(EXTRA_USER) as User
 
         val userPic = findViewById<ImageView>(R.id.titlePhoto)
         val handleTxt = findViewById<TextView>(R.id.handleView)
@@ -23,12 +28,21 @@ class MainActivity : AppCompatActivity() {
         val solvedProblemsBtn = findViewById<Button>(R.id.solvedProblemsBtn)
         val strengthsBtn = findViewById<Button>(R.id.strengthsBtn)
 
-        Picasso.with(this).load(user.getTitlePhoto()).into(userPic)
+        Picasso.with(this).load(user!!.getTitlePhoto()).into(userPic)
 
-        handleTxt.text = user.getHandle()
-        rankTxt.text = user.getRank()
-        solvedProblemsBtn.text = "Attempted ${user.submissions.size}"
-        ratingsBtn.text = user.ratingList[user.ratingList.size-1].toString()
+        handleTxt.text = user!!.getHandle()
+        rankTxt.text = user!!.getRank()
+        solvedProblemsBtn.text = "Attempted ${user!!.submissions.size}"
+        ratingsBtn.text = user!!.ratingList[user!!.ratingList.size-1].toString()
     }
+
+    fun startRatingActivity(view: View) {
+        val intent = Intent(this, RatingActivity::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            intent.putExtra(Intent.EXTRA_USER, user)
+        }
+        startActivity(intent)
+    }
+
 
 }
