dce9df90fb9cec06f772d3a0fff0f6fee0faa171
gandhi56 <gandhi21299@gmail.com>
2020-04-28 02:33:53 -0600

    adding more tables, POINT

--- app/src/main/java/com/example/rubberduck/CategoriesActivity.kt
+++ app/src/main/java/com/example/rubberduck/CategoriesActivity.kt
@@ -1,18 +1,28 @@
 package com.example.rubberduck
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ScrollView
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
+import kotlinx.android.synthetic.main.activity_categories.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class CategoriesActivity : AppCompatActivity() {
 
     var user: User? = null
+    val tableLayout by lazy{TableLayout(this)}
+    var scrollview: ScrollView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +31,7 @@ class CategoriesActivity : AppCompatActivity() {
         user = intent.getSerializableExtra(Intent.EXTRA_USER) as User
 
         val pieChart = findViewById<PieChart>(R.id.piechart)
+        scrollview = findViewById(R.id.tableScroll)
 
         val desc = Description()
         desc.text = "Categories statistics"
@@ -39,5 +50,55 @@ class CategoriesActivity : AppCompatActivity() {
 
         dataset.colors = ColorTemplate.JOYFUL_COLORS.toMutableList()
         pieChart.animateXY(1400, 1400)
+
+        createTable()
     }
+
+    private fun createTable(){
+
+        tableLayout.apply{
+            layout(10, 10, 0, 10)
+            // TODO
+//            background = Drawable.createFromPath("drawable/handle_view.xml")
+        }
+
+        for ((key, value) in user!!.classStats){
+            val row = TableRow(this)
+            row.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+
+            // add key
+            val keyTxt = TextView(this)
+            keyTxt.apply {
+                layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT)
+                text = key
+                textSize = 22F
+            }
+            row.addView(keyTxt)
+
+            // add value
+            val valueTxt = TextView(this)
+            valueTxt.apply {
+                layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT)
+                text = value.toString()
+                textSize = 22F
+            }
+            row.addView(valueTxt)
+            tableLayout.addView(row)
+        }
+        scrollview?.addView(tableLayout)
+    }
+
 }
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/rubberduck/SubmissionActivity.kt
+++ app/src/main/java/com/example/rubberduck/SubmissionActivity.kt
@@ -3,6 +3,11 @@ package com.example.rubberduck
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ScrollView
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.PieData
@@ -13,6 +18,8 @@ import com.github.mikephil.charting.utils.ColorTemplate
 class SubmissionActivity : AppCompatActivity() {
 
     var user: User? = null
+    val tableLayout by lazy{TableLayout(this)}
+    var scrollview: ScrollView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,7 @@ class SubmissionActivity : AppCompatActivity() {
         user = intent.getSerializableExtra(Intent.EXTRA_USER) as User
 
         val pieChart = findViewById<PieChart>(R.id.piechart)
+        scrollview = findViewById(R.id.verdictsScroll)
         val desc = Description()
         desc.text = "Submissions statistics"
         desc.textSize = 16f
@@ -38,5 +46,46 @@ class SubmissionActivity : AppCompatActivity() {
 
         dataset.colors = ColorTemplate.COLORFUL_COLORS.toMutableList()
         pieChart.animateXY(1400, 1400)
+
+        createTable()
+    }
+
+    private fun createTable(){
+        tableLayout.apply{
+            layout(10, 10, 0, 10)
+            // TODO
+//            background = Drawable.createFromPath("drawable/handle_view.xml")
+        }
+
+        for ((key, value) in user!!.verdictStats){
+            val row = TableRow(this)
+            row.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT)
+
+            // add key
+            val keyTxt = TextView(this)
+            keyTxt.apply {
+                layoutParams = TableRow.LayoutParams(
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT)
+                text = key
+                textSize = 22F
+            }
+            row.addView(keyTxt)
+
+            // add value
+            val valueTxt = TextView(this)
+            valueTxt.apply {
+                layoutParams = TableRow.LayoutParams(
+                    TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT)
+                text = value.toString()
+                textSize = 22F
+            }
+            row.addView(valueTxt)
+            tableLayout.addView(row)
+        }
+        scrollview?.addView(tableLayout)
     }
 }
