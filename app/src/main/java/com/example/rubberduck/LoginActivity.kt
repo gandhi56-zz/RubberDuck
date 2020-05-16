fc104a650e7a88a06979345e73c1ef6123552277
gandhi56 <gandhi21299@gmail.com>
2020-05-16 03:54:18 -0600

    POINT

--- app/src/main/java/com/example/rubberduck/LoginActivity.kt
+++ app/src/main/java/com/example/rubberduck/LoginActivity.kt
@@ -1,40 +1,17 @@
 package com.example.rubberduck
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
-import android.os.AsyncTask
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
-import android.widget.*
 import androidx.annotation.RequiresApi
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import com.example.rubberduck.HandleInput.EMPTY
-import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.fragment_login.*
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import org.json.JSONObject
-import kotlin.math.max
-
-enum class HandleInput{
-    WAIT, EMPTY, INVALID, OK
-}
-
-// https://codeforces.com/api/user.status?handle=Fefer_Ivan
 
 @Suppress("UNREACHABLE_CODE")
 class LoginActivity : AppCompatActivity() {
-
-    private lateinit var progBar: ProgressBar
-    private lateinit var handleText: EditText
-    private lateinit var handleState: HandleInput
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
