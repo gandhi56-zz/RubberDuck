24c39a3a0e54330fd3057a5b25442d31a18134c5
gandhi56 <gandhi21299@gmail.com>
2020-05-16 03:41:29 -0600

    login works, POINT

--- app/src/main/java/com/example/rubberduck/LoginFragment.kt
+++ app/src/main/java/com/example/rubberduck/LoginFragment.kt
@@ -1,24 +1,138 @@
 package com.example.rubberduck
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.view.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
+import kotlin.math.max
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
+
+    val user = User()
+
+    @SuppressLint("StaticFieldLeak")
+    internal inner class UserProfileRequest : AsyncTask<Context, Void, Boolean>(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+            usernameLogin.isEnabled = false
+            passwordLogin.isEnabled = false
+            loginBtn.isEnabled = false
+        }
+
+        override fun doInBackground(vararg params: Context): Boolean {
+            if (!userInfo())    return false
+            if (!userStatus())  return false
+            if (!userRating())  return false
+            return true
+        }
+
+        override fun onPostExecute(result: Boolean) {
+            super.onPostExecute(result)
+            if (result){
+                startMainActivity()
+            }
+            else{
+                println("Error logging in")
+            }
+            usernameLogin.isEnabled = true
+            passwordLogin.isEnabled = true
+            loginBtn.isEnabled = true
+        }
+
+        private fun sendHTTPRequest(url: String): String{
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            return response.body()?.string().toString()
+        }
+
+        private fun userInfo(): Boolean{
+            val json = sendHTTPRequest("https://codeforces.com/api/user.info?handles="
+                    + getHandle())
+            val jsonObj = JSONObject(json)
+            if (jsonObj.getString("status") == "FAILED")    return false
+            val resultArray = jsonObj.getJSONArray("result")
+            user.setHandle(resultArray.getJSONObject(0).getString("handle"))
+            user.setTitlePhoto("https:" + resultArray.getJSONObject(0)
+                .getString("titlePhoto"))
+
+            if (resultArray.getJSONObject(0).has("rank")){
+                user.setRank(resultArray.getJSONObject(0).getString("rank"))
+            }
+            println("User info received")
+            return true
+        }
+
+        private fun userStatus(): Boolean{
+            val json = sendHTTPRequest("https://codeforces.com/api/user.status?handle="
+                    + getHandle())
+            val jsonObj = JSONObject(json)
+            if (jsonObj.getString("status") == "FAILED")    return false
+            val resultArray = jsonObj.getJSONArray("result")
+
+            for (i in 0 until resultArray.length()) {
+                val sub = Submission()
+                sub.id = resultArray.getJSONObject(i).getInt("id")
+                if (!resultArray.getJSONObject(i).getJSONObject("problem").has("contestId"))    continue
+                if (!resultArray.getJSONObject(i).has("verdict"))   continue
+                sub.verdict = resultArray.getJSONObject(i).getString("verdict")
+                sub.problem.contestId = resultArray.getJSONObject(i).getJSONObject("problem")
+                    .getInt("contestId")
+                sub.problem.index = resultArray.getJSONObject(i).getJSONObject("problem")
+                    .getString("index")
+                sub.problem.name = resultArray.getJSONObject(i).getJSONObject("problem")
+                    .getString("name")
+                if (resultArray.getJSONObject(i).getJSONObject("problem").has("rating")){
+                    sub.problem.rating = resultArray.getJSONObject(i).getJSONObject("problem")
+                        .getInt("rating")
+                }
+                else{
+                    sub.problem.rating = 0
+                }
+                val tags = resultArray.getJSONObject(i).getJSONObject("problem")
+                    .getJSONArray("tags")
+                (0 until tags.length()).forEach{j ->
+                    sub.problem.tags.add(tags[j].toString())
+                    user.addClass(tags[j].toString())
+                }
+
+                user.addSubmission(sub.problem.contestId.toString() + sub.problem.index, sub)
+                user.lastSubmId = max(user.lastSubmId, sub.id)
+            }
+            println("user status received")
+            return true
+        }
+
+        private fun userRating(): Boolean{
+            val json = sendHTTPRequest("https://codeforces.com/api/user.rating?handle="
+                    + user.getHandle())
+            val jsonObj = JSONObject(json)
+            if (jsonObj.getString("status") == "FAILED")    return false
+            val resultArray = jsonObj.getJSONArray("result")
+            (0 until resultArray.length()).forEach {i ->
+                resultArray.getJSONObject(i)
+                val ratingChangeObj = RatingChange()
+                ratingChangeObj.contestId = resultArray.getJSONObject(i).getInt("contestId")
+                ratingChangeObj.contestName = resultArray.getJSONObject(i).getString("contestName")
+                ratingChangeObj.newRating = resultArray.getJSONObject(i).getInt("newRating")
+                ratingChangeObj.rank = resultArray.getJSONObject(i).getInt("rank")
+                user.ratingChangeList.add(ratingChangeObj)
+            }
+            return true
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,27 +142,42 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        view.loginBtn.setOnClickListener {
+            loginEventHandler()
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun loginEventHandler(){
+        println("Logging user in")
+        val username = usernameLogin.text.toString()
+        val password = passwordLogin.text.toString()
+        if (valid(username, password)){
+            UserProfileRequest().execute()
+        }
+        else{
+            Toast.makeText(context, "Invalid username or password", Toast.LENGTH_LONG).show()
+        }
     }
+
+    private fun valid(username: String, password: String): Boolean{
+        return true
+    }
+
+    private fun getHandle(): String{
+        return usernameLogin.text.toString()
+    }
+
+    private fun startMainActivity(){
+        val intent = Intent(activity, MainActivity::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            intent.putExtra(Intent.EXTRA_USER, user)
+        }
+        startActivity(intent)
+    }
+
+
 }
